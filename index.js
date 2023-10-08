c638630f2e3d0a774596924eaa9f168a5ac7b52a
Gabo <jvsoftassetstore@gmail.com>
2023-10-08 11:30:50 -0400

    fixed includes and added main route

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// render form
+app.get('/', (req, res) => {
+    res.render('form', {
+        title: 'Parse HTTP Get data',
+        data: req.query
+    });
+});
+
 app.listen(cfg.port, () => {
     console.log(`Example app listening at http://localhost:${cfg.port}`);
 });
